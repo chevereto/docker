321e3613972452979e7ed07cd90e4d633cf65225
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2021-10-07 19:15:30 -0300

    ando como dia jueves

--- chevereto.sh
+++ chevereto.sh
@@ -2,17 +2,15 @@
 set -e
 DIR="/var/www"
 WORKING_DIR="$DIR/html"
-mkdir -p /chevereto && mkdir -p /chevereto/download && mkdir -p /chevereto/installer
+CHEVERETO_INSTALLER_DOWNLOAD="https://github.com/chevereto/installer/releases/download/"
+mkdir -p /chevereto && mkdir -p /chevereto/download
 cd /chevereto/download
-curl -S -o installer.tar.gz -L "https://github.com/chevereto/installer/archive/${CHEVERETO_INSTALLER_TAG}.tar.gz"
-tar -xvzf installer.tar.gz
-mv -v installer-"${CHEVERETO_INSTALLER_TAG}"/* /chevereto/installer/
+curl -SOJL "${CHEVERETO_INSTALLER_DOWNLOAD}${CHEVERETO_INSTALLER_TAG}/installer.php"
 if [ -f "$WORKING_DIR/composer.json" ]; then
     echo "[SKIP] Installer provisioning"
 else
-    cd /chevereto/installer
-    php installer.php -a download -s $CHEVERETO_SOFTWARE -t=$CHEVERETO_TAG -l=$CHEVERETO_LICENSE
-    php installer.php -a extract -s $CHEVERETO_SOFTWARE -f chevereto-pkg-*.zip -p $WORKING_DIR
+    php installer.php -a download -t=$CHEVERETO_TAG -l=$CHEVERETO_LICENSE
+    php installer.php -a extract -f chevereto-pkg-*.zip -p $WORKING_DIR
 fi
 mkdir -p $WORKING_DIR/app
 touch $WORKING_DIR/app/settings.php
