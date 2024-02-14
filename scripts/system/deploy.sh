29a4bf20fe96d0023f71cbd416488ff8ea5f6acf
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-02-14 10:18:58 -0300

    s

--- scripts/system/deploy.sh
+++ scripts/system/deploy.sh
@@ -7,16 +7,16 @@ ADMIN_USER="${ADMIN_USER:-admin}"
 ADMIN_EMAIL="${ADMIN_EMAIL}"
 ADMIN_PASSWORD="$(openssl rand -base64 8)"
 
-NAMESPACE=${NAMESPACE} make provision &&
+NAMESPACE=${NAMESPACE} make --no-print-directory provision &&
     NAMESPACE=${NAMESPACE} \
         ADMIN_PASSWORD=${ADMIN_PASSWORD} \
         ADMIN_EMAIL=${ADMIN_EMAIL} \
-        ADMIN_USER=${ADMIN_USER} make install
+        ADMIN_USER=${ADMIN_USER} make --no-print-directory install
 
 echo ""
 echo "🚀 Deployment complete!"
 
-NAMESPACE=${NAMESPACE} make feedback--url
+NAMESPACE=${NAMESPACE} make --no-print-directory feedback--url
 
 echo "Login details"
 echo "--"
